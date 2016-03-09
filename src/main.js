5fa228c8ddbd0051d6e690bf2ffa66d6b55f0da4
Kateryna <kate.kotsar@gmail.com>
2016-03-09 22:28:23 +0100

    Add UI optimization for the mobile devices.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-require('bootstrap/dist/css/bootstrap.css')
+require('bootstrap/dist/css/bootstrap.css');
+var $ = jQuery = require('jquery');
+require('bootstrap/dist/js/bootstrap.js');
+
 require('./css/main.css');
-
 window.initMap = function initMap() {
-  require('./js/viewmodel.js');
+    require('./js/viewmodel.js');
 };
-
